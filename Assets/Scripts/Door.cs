a5819b4bf4bd04698a29c2914fb7c0578f1f16b6
Angel Alvarez <alvareza@mit.edu>
2018-04-22 13:34:00 -0400

    Created Assets for Door, Windows, Walls, and Jail Bars

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    public bool isOpen;
+    private bool doorClosed; // tracks true state of the door
+
+    public GameObject ColliderObject;
+
+	// Use this for initialization
+	void Start () {
+        isOpen = false;
+        doorClosed = true;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        CheckState();
+
+    }
+
+    public void ToggleDoorState() {
+        isOpen = !isOpen;
+        CheckState();
+    }
+    private void CheckState() {
+        if (isOpen)
+        {
+            // Door is supposed to be open
+            if (doorClosed) {
+                // Open the door
+                ColliderObject.GetComponent<BoxCollider2D>().enabled = false;
+                doorClosed = false;
+            }
+        }
+        else {
+            // Door is supposed to be closed
+            if (!doorClosed) {
+                //Close the door
+                ColliderObject.GetComponent<BoxCollider2D>().enabled = true;
+                doorClosed = true;
+            }
+        }
+    }
+}
